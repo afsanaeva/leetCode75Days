f937baa74a135380a51bc14e9b2e19b4f42c7b3a
afsanaeva <afsanaeva05@gmail.com>
2024-05-11 12:21:05 +0600

    problem solve of problem 1071, 1431

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetCode75Days
+
+In this repo i will upload the Leet code 75 days problems solve in JavaScript.
---
+++ day1_problem _1071.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+function gcdOfStrings(str1, str2) {
+    if (str1 + str2 !== str2 + str1) {
+        return "";
+    }
+    
+    const gcd = (a, b) => {
+        if (b === 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    };
+    
+    const length = gcd(str1.length, str2.length);
+    return str1.substring(0, length);
+}
