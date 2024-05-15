b7071af721e2f1ea3b99993ab35f0bc7df22e636
afsanaeva <afsanaeva05@gmail.com>
2024-05-15 13:41:10 +0600

    Create day8_problem_334.js
    solve 334 problem

---
+++ day8_problem_334.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let min = Number.MAX_VALUE; // Initialize min to the maximum possible value
+    let mid = Number.MAX_VALUE; // Initialize mid to the maximum possible value
+    
+    for (let num of nums) {
+        if (num <= min) {
+            // Update min if num is smaller than or equal to min
+            min = num;
+        } else if (num <= mid) {
+            // Update mid if num is smaller than or equal to mid
+            mid = num;
+        } else {
+            // Found a number greater than both min and mid
+            return true;
+        }
+    }
+    
+    // No triplet found
+    return false;
+};
