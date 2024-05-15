7dff186d9d9dec69d29b90cd37ce057e4de76fe0
afsanaeva <afsanaeva05@gmail.com>
2024-05-15 11:14:00 +0600

    problem 1768

---
+++ day1_problem_1768.js
@@ -0,0 +1,18 @@
+var mergeAlternately = function (word1, word2) {
+
+    const merge = [];
+    const n1 = word1.length;
+    const n2 = word2.length;
+    let i = 0;
+
+    while (i < n1 || i < n2) {
+        if (i < n1) {
+            merge.push(word1.charAt(i));
+        }
+        if (i < n2) {
+            merge.push(word2.charAt(i));
+        }
+        i++;
+    }
+    return merge.join('');
+};
